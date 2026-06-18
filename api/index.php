42c6eb94a584546ee4f5802210d5a932e389e162
Andrew Sykes <andrewsykes@btinternet.com>
2026-06-18 16:41:59 +0100

    comment_out

--- api/index.php
+++ api/index.php
@@ -3,6 +3,9 @@ require_once 'config.php';
 include_once FUNCTIONS;
 
 
+phpinfo();
+exit();
+
 $layout = 'pagelayout.html.php';
 $route = fixUri();
 $uri = empty($route) ? '' : implode('/', $route);
@@ -10,9 +13,12 @@ $home = 'home';
 //array of "actions" which need submit adding to string for processing forms; eg assignSubmit
 $posts = ['assign', 'create', 'contact', 'edit', 'login', 'manage', 'permissions', 'register', 'retrieve', 'retire', 'unarchive', 'relocate', 'swap'];
 $pp = $pages[$route[0]] ?? '';
+/*
 $website = new \PoloAfrica\PoloAfricaWebsite($pp);
 
 
 $entryPoint = new \Ninja\EntryPoint($website, $posts);
 $layoutVariables = $entryPoint->run($uri, $_SERVER['REQUEST_METHOD'], 'public', $home);
 echo $entryPoint->loadTemplate($layout, $layoutVariables);
+
+*/
